94cfcbd776ee6d7ddbe1aac6536f7b3f27676229
Danish Hafeez <danishhafeez992@gmail.com>
2025-07-02 00:59:03 +0500

    Optimizations

--- Watchers/src/App.vue
+++ Watchers/src/App.vue
@@ -18,7 +18,6 @@ const counter = ref(0)
 watch(counter, (newValue, oldValue) => {
   alert(`The old value is ${oldValue}: and the new Value is: ${newValue}`)
   console.log("Hello World");
-  
 })
 
 
